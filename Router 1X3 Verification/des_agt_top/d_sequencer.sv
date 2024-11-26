8340ecadd526f7480c1c3f08c4296c24aad1e0f2
Mahesh palaka <palakamahesh48325@gmail.com>
2024-11-26 10:36:45 +0530

    Delete Router 1X3 Verification directory

--- Router 1X3 Verification/des_agt_top/d_sequencer.sv
+++
@@ -1,6 +0,0 @@
-class d_sequencer extends uvm_sequencer#(dest_xtn);
-	`uvm_component_utils(d_sequencer)
-	function new(string name = "d_sequencer", uvm_component parent);
-		super.new(name, parent);
-	endfunction : new
-endclass : d_sequencer
--- Router 1X3 Verification/source_agt_top/source_cfg.sv
+++
@@ -1,8 +0,0 @@
-class source_cfg extends uvm_object;
-	`uvm_object_utils(source_cfg)
-	uvm_active_passive_enum is_active = UVM_ACTIVE;
-	virtual source_if vif;
-	function new(string name = "source_cfg");
-		super.new(name);
-	endfunction : new
-endclass : source_cfg
